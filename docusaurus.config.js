c27778b0aa770a07a8715769b5788866865073e4
Erick Cajamarca <168581102+elcaja5@users.noreply.github.com>
2024-07-31 22:52:25 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Aptein-Santa Ana',
+  tagline: 'Proyecto de Vinculación',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
